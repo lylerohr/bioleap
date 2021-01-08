a4ae961a9b9a39d1f00d2103221bf9fc37737631
Lyle <lyle@ssxd.io>
2021-01-08 17:04:53 -0500

    guess we’re not ignoring these

---
+++ sites/default/files/php/twig/5ff8d4975919f_commerce-wishlist-block.h_JvhgQpUyTc00dK-7B6MqR9zdb/Ulj7d5T8i6eROBAxJcRDgsbS5NrfFKWxQoSfJm0VhXg.php
@@ -0,0 +1,102 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/commerce_wishlist/templates/commerce-wishlist-block.html.twig */
+class __TwigTemplate_f61d3dc479eb26a65b5861e2723dfbd5d5c44fff78a3ac5b25d126a605e1ead2 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 15
+        echo "<div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "wishlist-block"], "method", false, false, true, 15), 15, $this->source), "html", null, true);
+        echo ">
+  <a class=\"wishlist-block__link\" href=\"";
+        // line 16
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null), 16, $this->source), "html", null, true);
+        echo "\">
+    <span class=\"wishlist-block__count\">";
+        // line 17
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["count_text"] ?? null), 17, $this->source), "html", null, true);
+        echo "</span>
+  </a>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/commerce_wishlist/templates/commerce-wishlist-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  48 => 17,  44 => 16,  39 => 15,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/commerce_wishlist/templates/commerce-wishlist-block.html.twig", "/app/modules/contrib/commerce_wishlist/templates/commerce-wishlist-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 15);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
