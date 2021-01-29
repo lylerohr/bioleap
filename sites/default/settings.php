b6faf74633b228c93e4c3cdcb2d96bc8eb43ec2b
Lyle <lrohr@tahoepartners.com>
2021-01-29 11:21:17 -0500

    Test

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -780,28 +780,28 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 //
-// $databases['default']['default'] = array (
-//   'database' => 'drupal9',
-//   'username' => 'drupal9',
-//   'password' => 'drupal9',
-//   'prefix' => '',
-//   'host' => 'database',
-//   'port' => '3306',
-//   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-//   'driver' => 'mysql',
-// );
-// $settings['config_sync_directory'] = 'sites/default/files/config_WYQKXA0yvwTrdvb01tnyBzQJpv1oT86ilJ8uxmkOo5wYyblI_Aiw2Oj0PUFHP0nAi1SBZtfCTQ/sync';
-
-
-
 $databases['default']['default'] = array (
-  'database' => 'bioleap',
-  'username' => 'bioleapdbadmin',
-  'password' => '3f.RMReXMa9ufoJv3.eYw7dZa!.WWVzMiZw-',
+  'database' => 'drupal9',
+  'username' => 'drupal9',
+  'password' => 'drupal9',
   'prefix' => '',
-  'host' => 'localhost',
+  'host' => 'database',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
 $settings['config_sync_directory'] = 'sites/default/files/config_WYQKXA0yvwTrdvb01tnyBzQJpv1oT86ilJ8uxmkOo5wYyblI_Aiw2Oj0PUFHP0nAi1SBZtfCTQ/sync';
+
+
+//
+// $databases['default']['default'] = array (
+//   'database' => 'bioleap',
+//   'username' => 'bioleapdbadmin',
+//   'password' => '3f.RMReXMa9ufoJv3.eYw7dZa!.WWVzMiZw-',
+//   'prefix' => '',
+//   'host' => 'localhost',
+//   'port' => '3306',
+//   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+//   'driver' => 'mysql',
+// );
+// $settings['config_sync_directory'] = 'sites/default/files/config_WYQKXA0yvwTrdvb01tnyBzQJpv1oT86ilJ8uxmkOo5wYyblI_Aiw2Oj0PUFHP0nAi1SBZtfCTQ/sync';
